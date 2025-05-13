156eb5fa458cc142ff0c2a57d6b7a3cc6d6c1473
Yukta Koralla <yukta.koralla5@gmail.com>
2025-05-13 19:59:45 +0200

    basic app setup.

---
+++ cardano-dashboard/src/components/BubbleMap.jsx
@@ -0,0 +1,13 @@
+import * as d3 from "d3"
+
+export default function BubbleMap({
+    data,
+    width = 640,
+    height = 400,
+    marginTop = 20,
+    marginRight = 20,
+    marginBottom = 20,
+    marginLeft = 20
+}) {
+    const x = d3.sc
+}
---
+++ cardano-dashboard/src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { clsx } from 'clsx'
+import * as d3 from "d3";
+import { data } from "../data"
+
+export default function Main() {
+    // State values
+    
+    // Static values
+    
+
+    // Derived values
+    
+
+    return(
+        <main>
+            <header>
+                <h1>Cardano dashboard</h1>
+                <p className="intro">Bubble map showing staking pools of Cardano!</p>
+            </header>
+        </main>
+    )
+}
