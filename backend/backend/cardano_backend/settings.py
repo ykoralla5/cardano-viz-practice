b0ee84ff252fdaabcd8ea066c6aa5e748b7604d3
Yukta Koralla <yukta.koralla5@gmail.com>
2025-06-04 16:39:30 +0200

    sqlite3 to psql

--- backend/backend/cardano_backend/settings.py
+++ backend/backend/cardano_backend/settings.py
@@ -80,21 +80,14 @@ WSGI_APPLICATION = 'cardano_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cardano_backend',
+        'USER' : 'yukta',
+        'PASSWORD' : 'password',
+        'HOST': 'localhost',
+        'PORT' : '5432'
     }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "cexplorer",
-#         "USER": "yukta",
-#         "PASSWORD": "password",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
 
 
 # Password validation
